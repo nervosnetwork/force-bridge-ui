50f225ce030d6f005247e2534f4ea49bc9e48b22
homura <homura.dev@gmail.com>
2021-04-21 14:26:26 +0800

    fix(commons): fix predicate error

--- packages/commons/src/xchain/helper.ts
+++ packages/commons/src/xchain/helper.ts
@@ -1,11 +1,8 @@
 import { boom } from '../errors';
 import { AssetLike } from '../types/module';
-import { NetworkTypes } from '../types/network';
 
 // typescript helper function
-export function createAssetPredicate<T extends NetworkTypes>(
-  pred: (x: AssetLike) => boolean,
-): <X>(asset: X) => asset is AssetLike<T> & X {
+export function createAssetPredicate<T>(pred: (x: AssetLike) => boolean): <X>(asset: X) => asset is T & X {
   if (typeof pred !== 'function') boom('param of `createAssetPredicate` must be a function');
-  return pred as <X>(asset: X) => asset is AssetLike<T> & X;
+  return pred as <X>(asset: X) => asset is T & X;
 }
