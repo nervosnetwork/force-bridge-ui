c7cbd5813e2fbc06dcc1c37265c098bc035eab4b
homura <homura.dev@gmail.com>
2021-04-21 19:39:51 +0800

    refactor(ui): move errors to commons

--- apps/ui/src/interfaces/errors/index.ts
+++ apps/ui/src/interfaces/errors/index.ts
@@ -1,12 +1,1 @@
-export function unimplemented(): never {
-  throw new Error('unimplemented method');
-}
-
-type BoomOptions = string | (() => never);
-
-export function boom(option?: BoomOptions): never {
-  if (typeof option === 'function') return option();
-  if (typeof option === 'string') throw new Error(option);
-
-  throw new Error('An unknown error has occurred');
-}
+export * from '@force-bridge/commons/lib/errors';
