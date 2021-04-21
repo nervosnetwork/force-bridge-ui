464acd04b99e128e99fa9d1cc23f2566452d3745
homura <homura.dev@gmail.com>
2021-04-21 19:35:44 +0800

    feat(ui): beauty WalletConnectorButton

--- apps/ui/src/components/WalletConnector/WalletConnectorButton.tsx
+++ apps/ui/src/components/WalletConnector/WalletConnectorButton.tsx
@@ -1,7 +1,31 @@
 import { Button, ButtonProps } from 'antd';
 import React, { useMemo } from 'react';
+import styled from 'styled-components';
 import { ConnectStatus } from 'interfaces/WalletConnector';
 import { useForceBridge } from 'state/global';
+import { truncateMiddle } from 'utils';
+
+const StyledWalletConnectButton = styled(Button)`
+  border: none;
+  color: ${(props) => props.theme.palette.common.black};
+  font-weight: 700;
+  background: linear-gradient(93.35deg, #b8f0d5 3.85%, #b8f0ed 100%);
+
+  :hover {
+    color: ${(props) => props.theme.palette.common.black};
+    background: linear-gradient(93.35deg, #b8f0d5 3.85%, #b8f0ed 100%);
+  }
+
+  :active {
+    color: ${(props) => props.theme.palette.common.black};
+    background: linear-gradient(93.35deg, #b8f0d5 3.85%, #b8f0ed 100%);
+  }
+
+  :focus {
+    color: ${(props) => props.theme.palette.common.black};
+    background: linear-gradient(93.35deg, #b8f0d5 3.85%, #b8f0ed 100%);
+  }
+`;
 
 export interface WalletConnectorButtonProps extends ButtonProps {
   disconnectedContent?: React.ReactNode;
@@ -23,7 +47,7 @@ export const WalletConnectorButton: React.FC<WalletConnectorButtonProps> = (prop
     if (walletConnectStatus === ConnectStatus.Connecting) return connectingContent;
     if (!signer) return connectingContent;
 
-    if (userIdentityMode === 'alwaysNervos') return signer.identityNervos();
+    if (userIdentityMode === 'alwaysNervos') return truncateMiddle(signer.identityNervos(), 10);
 
     return signer.identityOrigin();
   }, [walletConnectStatus, disconnectedContent, signer, connectingContent, userIdentityMode]);
@@ -36,8 +60,8 @@ export const WalletConnectorButton: React.FC<WalletConnectorButtonProps> = (prop
     walletConnectStatus === ConnectStatus.Connecting || (walletConnectStatus === ConnectStatus.Connected && !signer);
 
   return (
-    <Button {...buttonProps} onClick={onClick} loading={connecting}>
+    <StyledWalletConnectButton {...buttonProps} onClick={onClick} loading={connecting}>
       {buttonContent}
-    </Button>
+    </StyledWalletConnectButton>
   );
 };
