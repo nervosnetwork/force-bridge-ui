6ae81a0a892f3a6d024fd5f86d9c186cddce0eca
homura <homura.dev@gmail.com>
2021-04-29 17:36:06 +0800

    fix(ui): TransactionLink for exploring tx

---
+++ apps/ui/src/components/TransactionLink/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from 'antd';
+import React from 'react';
+
+interface TransactionLinkProps {
+  network: string;
+  txId: string;
+}
+
+export const TransactionLink: React.FC<TransactionLinkProps> = (props) => {
+  const { network, txId } = props;
+  // TODO refactor to UIModule
+  let href = '';
+  if (network === 'Nervos') href = process.env.REACT_APP_TX_EXPLORER_NERVOS + `/${txId}`;
+  if (network === 'Ethereum') href = process.env.REACT_APP_TX_EXPLORER_ETHEREUM + `${txId}`;
+
+  return (
+    <Button type="link">
+      <a href={href} target="_blank" rel="noreferrer">
+        {props.children}
+      </a>
+    </Button>
+  );
+};
--- apps/ui/src/types/index.d.ts
+++ apps/ui/src/types/index.d.ts
@@ -2,6 +2,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
+
     REACT_APP_BRIDGE_RPC_URL: string;
+
+    REACT_APP_TX_EXPLORER_NERVOS: string;
+    REACT_APP_TX_EXPLORER_ETHEREUM: string;
   }
 }
