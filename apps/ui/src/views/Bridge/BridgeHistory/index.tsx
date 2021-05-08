4ef2d32b3f4e5156e99b8e1d8e6933bc091656f0
mkxbl <vyclzn@gmail.com>
2021-05-08 19:06:00 +0800

    finish history component

--- apps/ui/src/views/Bridge/BridgeHistory/index.tsx
+++ apps/ui/src/views/Bridge/BridgeHistory/index.tsx
@@ -1,10 +1,11 @@
 import { API, Asset } from '@force-bridge/commons';
-import { Table } from 'antd';
+import { Button, Col, Row, Table, Tooltip } from 'antd';
 import { ColumnsType } from 'antd/lib/table/interface';
 import dayjs from 'dayjs';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
+import { TransactionLink } from '../../../components/TransactionLink';
 import { HumanizeAmount } from 'components/AssetAmount';
 import { StyledCardWrapper } from 'components/Styled';
 import { useForceBridge } from 'state';
@@ -71,30 +72,107 @@ export const BridgeHistory: React.FC<BridgeHistoryProps> = (props) => {
             <HumanizeAmount showSymbol asset={record.txSummary.toAsset} />
           </div>
           <div className="date">
-            {dayjs(record.txSummary.toTransaction?.timestamp || record.txSummary.fromTransaction.timestamp).format(
-              'YYYY-MM-DD HH:mm:ss',
-            )}
+            <Tooltip
+              title={
+                <div>
+                  <div>
+                    <TransactionLink
+                      network={record.txSummary.fromAsset.network}
+                      txId={record.txSummary.fromTransaction.txId}
+                    >
+                      Explore {record.txSummary.fromAsset.network} Tx
+                    </TransactionLink>
+                  </div>
+                  {record.txSummary?.toTransaction?.txId && (
+                    <div>
+                      <TransactionLink
+                        network={record.txSummary.toAsset.network}
+                        txId={record.txSummary.toTransaction.txId}
+                      >
+                        Explore {record.txSummary.toAsset.network} Tx
+                      </TransactionLink>
+                    </div>
+                  )}
+                </div>
+              }
+            >
+              {dayjs(record.txSummary.toTransaction?.timestamp || record.txSummary.fromTransaction.timestamp).format(
+                'YYYY-MM-DD HH:mm:ss',
+              )}
+            </Tooltip>
           </div>
         </div>
       ),
     },
   ];
 
+  const [historyKind, setHistoryKind] = useState<'Pending' | 'Successful'>('Pending');
+  const historyData = useMemo(() => {
+    if (!query.data) return [];
+    return query.data.filter((item) => {
+      return item.status === historyKind;
+    });
+  }, [query, historyKind]);
+
   return (
     <BridgeHistoryWrapper>
       <header>
         <strong>History</strong>
       </header>
-      {/*TODO fee calc*/}
-      {/*<Row gutter={12} style={{ margin: '16px 0' }}>*/}
-      {/*  <Col span={12}>*/}
-      {/*    <Button block>Pending</Button>*/}
-      {/*  </Col>*/}
-      {/*  <Col span={12}>*/}
-      {/*    <Button block>Succeed</Button>*/}
-      {/*  </Col>*/}
-      {/*</Row>*/}
-      <Table size="small" columns={columns} dataSource={query.data || []} pagination={{ pageSize: 5 }} />
+      <Row gutter={12} style={{ margin: '16px 0' }}>
+        <Col span={12}>
+          <Button
+            block
+            type={historyKind === 'Pending' ? 'primary' : undefined}
+            onClick={() => {
+              setHistoryKind('Pending');
+            }}
+          >
+            Pending
+          </Button>
+        </Col>
+        <Col span={12}>
+          <Button
+            block
+            type={historyKind === 'Successful' ? 'primary' : undefined}
+            onClick={() => {
+              setHistoryKind('Successful');
+            }}
+          >
+            Succeed
+          </Button>
+        </Col>
+      </Row>
+      <Table
+        size="small"
+        columns={columns}
+        expandable={{
+          expandedRowRender: (record) => (
+            <div>
+              <div>
+                <TransactionLink
+                  network={record.txSummary.fromAsset.network}
+                  txId={record.txSummary.fromTransaction.txId}
+                >
+                  Explore {record.txSummary.fromAsset.network} Tx
+                </TransactionLink>
+              </div>
+              {record.txSummary?.toTransaction?.txId && (
+                <div>
+                  <TransactionLink
+                    network={record.txSummary.toAsset.network}
+                    txId={record.txSummary.toTransaction.txId}
+                  >
+                    Explore {record.txSummary.toAsset.network} Tx
+                  </TransactionLink>
+                </div>
+              )}
+            </div>
+          ),
+        }}
+        dataSource={historyData}
+        pagination={{ pageSize: 5 }}
+      />
     </BridgeHistoryWrapper>
   );
 };
