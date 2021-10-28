20de23f6712a7dc1f8eab04890f0ae024802a194
mkxbl <33241113+mkxbl@users.noreply.github.com>
2021-10-28 12:24:47 +0800

    refactor: generateGodwokenAddress (#63)

--- apps/ui/src/views/Bridge/Ethereum/BridgeOperation/utils.ts
+++ apps/ui/src/views/Bridge/Ethereum/BridgeOperation/utils.ts
@@ -1,15 +1,37 @@
-import { Script } from '@ckb-lumos/base';
-import { generateAddress } from '@ckb-lumos/helpers';
+import bech32 from 'bech32';
 
 export function toGodwokenAddress(ethAddress: string): string {
   if (!process.env.REACT_APP_GODWOKEN_ETH_LOCKHASH) throw new Error('env REACT_APP_GODWOKEN_ETH_LOCKHASH not set');
   if (!process.env.REACT_APP_GODWOKEN_ROLLUP_TYPEHASH)
     throw new Error('env REACT_APP_GODWOKEN_ROLLUP_TYPEHASH not set');
-  const script: Script = {
+
+  const script = {
     code_hash: process.env.REACT_APP_GODWOKEN_ETH_LOCKHASH,
     hash_type: 'type',
     args: process.env.REACT_APP_GODWOKEN_ROLLUP_TYPEHASH + ethAddress.slice(2),
   };
   const addressPrefix = process.env.REACT_APP_CKB_CHAIN_ID === '0' ? 'ckb' : 'ckt';
-  return generateAddress(script, { config: { PREFIX: addressPrefix, SCRIPTS: {} } });
+
+  const data = [];
+  data.push(script.hash_type === 'type' ? 4 : 2);
+  data.push(...hexToByteArray(script.code_hash));
+  data.push(...hexToByteArray(script.args));
+  const words = bech32.toWords(data);
+  const BECH32_LIMIT = 1023;
+  return bech32.encode(addressPrefix, words, BECH32_LIMIT);
+}
+
+function hexToByteArray(h: string): number[] {
+  if (!/^(0x)?([0-9a-fA-F][0-9a-fA-F])*$/.test(h)) {
+    throw new Error('Invalid hex string!');
+  }
+  if (h.startsWith('0x')) {
+    h = h.slice(2);
+  }
+  const array = [];
+  while (h.length >= 2) {
+    array.push(parseInt(h.slice(0, 2), 16));
+    h = h.slice(2);
+  }
+  return array;
 }
