ccb5c25eaf1753796a30e43c9a640f84187ade8e
mkxbl <vyclzn@gmail.com>
2021-11-19 15:50:27 +0800

    feat: support private chain

--- apps/ui/src/views/Bridge/Ethereum/index.tsx
+++ apps/ui/src/views/Bridge/Ethereum/index.tsx
@@ -1,4 +1,14 @@
-import PWCore, { CHAIN_SPECS, ChainID, EthProvider, PwCollector } from '@lay2/pw-core';
+import PWCore, {
+  CellDep,
+  CHAIN_SPECS,
+  ChainID,
+  DepType,
+  EthProvider,
+  HashType,
+  OutPoint,
+  PwCollector,
+  Script,
+} from '@lay2/pw-core';
 import React, { useEffect, useState } from 'react';
 import { BridgeOperationForm } from './BridgeOperation';
 import { ChainIdWarning } from './ChainIdWarning';
@@ -29,7 +39,16 @@ const EthereumBridge: React.FC = () => {
 
     api.getBridgeConfig().then(async (config) => {
       const ckbRpcUrl = process.env.REACT_APP_CKB_RPC_URL;
-      const pwChainId = ckbChainID === 0 ? ChainID.ckb : ChainID.ckb_testnet;
+      const pwChainId = (() => {
+        switch (ckbChainID) {
+          case 0:
+            return ChainID.ckb;
+          case 1:
+            return ChainID.ckb_testnet;
+          case 2:
+            return ChainID.ckb_dev;
+        }
+      })();
 
       const wallet = new EthereumWalletConnector({
         ckbRpcUrl: ckbRpcUrl,
@@ -39,12 +58,30 @@ const EthereumBridge: React.FC = () => {
 
       setWallet(wallet);
 
+      const getDevConfig = () => {
+        const devConfig = CHAIN_SPECS.Aggron;
+        devConfig.pwLock = {
+          cellDep: new CellDep(
+            process.env.REACT_APP_PWLOCK_DEP_TYPE === 'code' ? DepType.code : DepType.depGroup,
+            new OutPoint(process.env.REACT_APP_PWLOCK_OUTPOINT_TXHASH, process.env.REACT_APP_PWLOCK_OUTPOINT_INDEX),
+          ),
+          script: new Script(
+            process.env.REACT_APP_PWLOCK_CODE_HASH,
+            '0x',
+            process.env.REACT_APP_PWLOCK_HASH_TYPE === 'type' ? HashType.type : HashType.data,
+          ),
+        };
+        return devConfig;
+      };
+
+      const pwConfig = ckbChainID === 2 ? getDevConfig() : [CHAIN_SPECS.Lina, CHAIN_SPECS.Aggron][ckbChainID];
+
       await new PWCore(ckbRpcUrl).init(
         new EthProvider(),
         new PwCollector(ckbRpcUrl),
         // FIXME pw-lock has a bug here, remove the type convert after pw-core upgrade to 0.4.x
         (String(pwChainId) as unknown) as ChainID,
-        [CHAIN_SPECS.Lina, CHAIN_SPECS.Aggron][ckbChainID],
+        pwConfig,
       );
 
       // FIXME remove me when pw-core upgrade to 0.4.x
