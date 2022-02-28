f3eb0c6749cfa0e0add5f378f82da80fe991944e
mkxbl <vyclzn@gmail.com>
2022-02-28 14:53:18 +0800

    fix: lint

--- apps/ui/src/xchain/eth/EthWalletSigner.ts
+++ apps/ui/src/xchain/eth/EthWalletSigner.ts
@@ -51,7 +51,8 @@ export class EthWalletSigner extends AbstractWalletSigner<EthereumNetwork> {
     let skeleton = helpers.objectToTransactionSkeleton(raw);
     const messageToSign = skeleton.signingEntries.get(0)?.message;
     if (!messageToSign) throw new Error('Invalid burn tx: no signingEntries');
-    const paramsToSign = window.ethereum!.isSafePal ? [messageToSign] : [this.identityXChain(), messageToSign];
+    if (!window.ethereum) throw new Error('Could not find ethereum wallet');
+    const paramsToSign = window.ethereum.isSafePal ? [messageToSign] : [this.identityXChain(), messageToSign];
     let sigs = await this.provider.send('personal_sign', paramsToSign);
 
     let v = Number.parseInt(sigs.slice(-2), 16);
