3989c54055f0936ae8de82d53aa8844ccaa3e6c8
pygman <pygman@aliyun.com>
2022-09-15 08:34:36 +0800

    feat: mask for the merge of ethereum

--- apps/ui/src/views/index.tsx
+++ apps/ui/src/views/index.tsx
@@ -50,7 +50,7 @@ export const AppView: React.FC = () => {
       const startMerge6 = dayjs.utc('2022-09-06 11:00', 'YYYY-MM-DD HH:mm');
       const endMerge6 = startMerge6.add(2, 'hours');
       const startMerge15 = dayjs.utc('2022-09-15 00:00', 'YYYY-MM-DD HH:mm');
-      const endMerge15 = startMerge15.add(6, 'hours');
+      const endMerge15 = startMerge15.add(14, 'hours');
       if (
         (utcTime.isAfter(startMerge6) && utcTime.isBefore(endMerge6)) ||
         (utcTime.isAfter(startMerge15) && utcTime.isBefore(endMerge15))
